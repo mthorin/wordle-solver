77a9ad5b09784532e5aad7bc7bfeb1056daee580
mthorin <mthorin@byu.edu>
2025-01-05 01:35:31 -0600

    feat(framework): layed out functions stubs and main loop

--- main.py
+++ main.py
@@ -4,9 +4,30 @@ def load_words():
 
     return valid_words
 
+def update_correct(words, letters):
+    pass
+
+def update_incorrect(words, letters):
+    pass
+
+def update_yellow(words, letters):
+    pass
+
+def generate_best_word(words, correct, incorrect, yellow):
+    pass
+
 def main():
     words = load_words()
 
+    correct_letters = [None, None, None, None, None]
+    incorrect_letters = []
+    yellow_letters = [[],[],[],[],[]]
+
+    while None in correct_letters:
+        update_correct(words, correct_letters)
+        update_incorrect(words, incorrect_letters)
+        update_yellow(words, yellow_letters)
+        generate_best_word(words, correct_letters, incorrect_letters, yellow_letters)
 
 if __name__ == '__main__':
     main()
