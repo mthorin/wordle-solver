20f61250fac2eee6d81267333ea4e6749d955e64
mthorin <mthorin@byu.edu>
2025-01-07 00:31:13 -0600

    feat(update_incorrect): wrote function for checking and removing words with incorrect letters

--- main.py
+++ main.py
@@ -13,15 +13,23 @@ def check_and_remove(words, check):
     for word in to_remove:
         words.remove(word)
 
+def get_input(in_string):
+    new = None
+    while not new:
+        new = input(in_string)
+        if len(new) != 5:
+            print("Invalid input length! Try again.")
+            new = None
+    return new
+
 def update_correct(words, letters):
+    print("")
     print(f"Correct letters previously confirmed:            {letters[0] if letters[0] else "_"}{letters[1] if letters[1] else "_"}{letters[2] if letters[2] else "_"}{letters[3] if letters[3] else "_"}{letters[4] if letters[4] else "_"}")
-    new = input("Enter new correct letters (use _ if no change) : ")
+    new = get_input("Enter new correct letters (use _ if no change) : ")
     
     for i in range (5):
-        if new[i] == "_":
-            continue
         letter = new[i].lower()
-        if letter.isalpha():
+        if letter.isalpha() and not letters[i]:
             letters[i] = letter
             check_and_remove(words, lambda word: word[i] != letter)
 
@@ -29,7 +37,18 @@ def update_correct(words, letters):
 
 
 def update_incorrect(words, letters):
-    pass
+    print("")
+    print(f"Incorrect letters previously confirmed: {letters}")
+    new = input("Enter incorrect letters: ")
+    
+    for char in new:
+        letter = char.lower()
+        if letter.isalpha():
+            if letter not in letters:
+                letters.append(letter)
+                check_and_remove(words, lambda word: letter in word)
+
+    print(words)
 
 def update_yellow(words, letters):
     pass
@@ -46,8 +65,8 @@ def main():
 
     while None in correct_letters:
         update_correct(possible_words, correct_letters)
-        update_incorrect(possible_words, incorrect_letters)
         update_yellow(possible_words, yellow_letters)
+        update_incorrect(possible_words, incorrect_letters)
         generate_best_word(possible_words, correct_letters, incorrect_letters, yellow_letters)
 
 if __name__ == '__main__':
